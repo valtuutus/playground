e1af1cf03e9f8aeebc5d3bc511475f84f2ab5b86
Yan Pitangui <yanpitangui@gmail.com>
2025-03-28 21:01:44 -0300

    trying to understand the error

--- Valtuutus.Playground/Program.cs
+++ Valtuutus.Playground/Program.cs
@@ -10,7 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSysinfocus(true);
+builder.Services.AddSysinfocus(false);
 builder.Services.AddValtuutusCore("entity user {}")
     .AddInMemory();
 
